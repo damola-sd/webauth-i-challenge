38aa3f8b8f5a06506c76f4409f81ad360a4e2b2b
Damola Adewunmi <damolasd@gmail.com>
2019-07-23 13:23:20 +0100

    MVP Complete: Added endpoints as required

---
+++ auth/auth-router.js
@@ -0,0 +1,59 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+
+const Users = require('../database/Models/user-model');
+
+// for endpoints beginning with /api/auth
+router.post('/register', (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.get('/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.send('Error. There is no user to log out');
+      } else {
+        res.send(`You've been logged out`);
+      }
+    });
+  } else {
+    res.end();
+  }
+});
+
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        // create a session holding session id and user info
+        // the response here is going to take a SetCookie with the session id
+        // on subsequent requests, thanks to the cookie that always gets sent from client
+        // express will be able to see if a session exists for that session id
+        req.session.user = user;
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,20 +1,55 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const knex = require('knex');
+
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
+const knexConfig = knex(require('./knexfile').development);
 
 const Users = require('./database/Models/user-model');
+const authRouter = require('./auth/auth-router');
+const restricted = require('./auth/restricted');
+
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
-
+server.use(session({
+    name: 'sessionId', // name of the cookie
+    secret: 'keep it secret, keep it long', // we intend to encrypt
+    cookie: {
+        maxAge: 1000 * 60 * 60,
+        secure: false,
+        httpOnly: true,
+    },
+    resave: false,
+    saveUninitialized: true,
+    // extra chunk of config
+    store: new KnexSessionStore({
+        knex: knexConfig, // configured instance of knex
+        tablename: 'sessions', // table that will store sessions inside the db, name it anything you want
+        sidfieldname: 'sid', // column that will hold the session id, name it anything you want
+        createtable: true, // if the table does not exist, it will create it automatically
+        clearInterval: 1000 * 60 * 60, // time it takes to check for old sessions and remove them from the database to keep it clean and performant
+    }),
+}));
+server.use('/api/auth', authRouter);
 
 server.get('/', (req, res) => {
     res.send("Testing Server");
-  });
+});
 
 
-const port = process.env.PORT || 5000;
+server.get('/api/users', restricted, (req, res) => {
+    Users.find()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => res.send(err));
+})
+
+const port = process.env.PORT || 3000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
